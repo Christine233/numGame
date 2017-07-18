a0b3cb5aae4f880cc1c9769160d6170756be833b
Christine233 <1040551136@qq.com>
2017-07-19 00:15:26 +0800

    second commit

--- main/main.js
+++ main/main.js
@@ -1,19 +1,36 @@
 'use strict';
+// 第一版
+// function numGame(system,user) {
+//   let a = 0, b = 0;
+//   for(let i in system){
+//     for(let j in user){
+//       if(i == j){
+//         if(system[i] === user[j]){
+//           a++;
+//         }
+//       }else{
+//         if(system[i] === user[j]){
+//           b++;
+//         }
+//       }
+//     }
+//   }
+//   return `${a}A${b}B`;
+// }
 
+// 优化版
 function numGame(system,user) {
   let a = 0, b = 0;
+  //let systemArr = system.split('');
+  //let userArr = user.split('');
   for(let i in system){
-    for(let j in user){
-      if(i == j){
-        if(system[i] === user[j]){
-          a++;
-        }
-      }else{
-        if(system[i] === user[j]){
-          b++;
-        }
-      }
+    if(system.indexOf(user[i]) != -1){
+      b++;
+    }
+    if(system[i] === user[i]){
+      a++;
     }
   }
+  b = b - a;
   return `${a}A${b}B`;
 }
