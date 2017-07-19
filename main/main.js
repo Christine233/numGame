8713d05f1fbc557a5a9adf51b067e41b74be5561
Christine233 <1040551136@qq.com>
2017-07-20 02:02:04 +0800

    third commit

--- main/main.js
+++ main/main.js
@@ -19,10 +19,13 @@
 // }
 
 // 优化版
-function numGame(system,user) {
+const readlineSync = require('readline-sync');
+function numGame() {
+  let system = this.systemRandomNum();
+  let user = readlineSync.question(`Please input your guessed number: `);
   let a = 0, b = 0;
-  //let systemArr = system.split('');
-  //let userArr = user.split('');
+//  let systemArr = system.split();
+//  let userArr = user.split();
   for(let i in system){
     if(system.indexOf(user[i]) != -1){
       b++;
@@ -34,3 +37,24 @@ function numGame(system,user) {
   b = b - a;
   return `${a}A${b}B`;
 }
+
+function systemRandomNum() {
+    let flag = 0;
+    let system = 0;
+    while (flag == 0){
+        let newArr = [];
+        system = Math.floor(Math.random()*1000+1000);
+        let systemArr = system.toString().split('');
+        for(let i in systemArr){
+            if(newArr.indexOf(systemArr[i]) == -1){
+              newArr.push(systemArr[i]);
+            }
+        }
+        if(newArr.length == systemArr.length){
+          flag = 1;
+        }
+    }
+    return system;
+}
+module.exports.systemRandomNum = systemRandomNum;
+module.exports.numGame = numGame;
